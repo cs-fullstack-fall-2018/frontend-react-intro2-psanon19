97f37470ed365469b3f7399a13f08fa22d623de4
Pelane Sanon <p_sanon@aol.com>
2018-08-29 11:26:53 -0500

    Check in

--- forth_app/src/App.js
+++ forth_app/src/App.js
@@ -10,11 +10,13 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
+
         </header>
+
         <Netflix movieInfo= {
-            [{movieName:"Your Name.",yearReleased:2016, genre: " Animation, Drama, Fantasy", src:"https://m.media-amazon.com/images/M/MV5BODRmZDVmNzUtZDA4ZC00NjhkLWI2M2UtN2M0ZDIzNDcxYThjL2ltYWdlXkEyXkFqcGdeQXVyNTk0MzMzODA@._V1_UX182_CR0,0,182,268_AL_.jpg", wiki:"https://www.imdb.com/title/tt5311514/"},
-                {movieName:"Annihilation",yearReleased:2018, genre: "Adventure, Drama, Horror ", src:"https://m.media-amazon.com/images/M/MV5BMTk2Mjc2NzYxNl5BMl5BanBnXkFtZTgwMTA2OTA1NDM@._V1_UX182_CR0,0,182,268_AL_.jpg", wiki:"https://www.imdb.com/title/tt2798920/"},
-                    {movieName:"Back to the Future",yearReleased:19850, genre: "Adventure, Comedy, Sci-Fi", src:"https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UX182_CR0,0,182,268_AL_.jpg", wiki:"https://www.imdb.com/title/tt0088763/"}
+            [{movieName:"Your Name.",yearReleased:2016, genre: " Animation, Drama, Fantasy", src:"https://m.media-amazon.com/images/M/MV5BODRmZDVmNzUtZDA4ZC00NjhkLWI2M2UtN2M0ZDIzNDcxYThjL2ltYWdlXkEyXkFqcGdeQXVyNTk0MzMzODA@._V1_UX182_CR0,0,182,268_AL_.jpg", wiki:"https://www.imdb.com/title/tt5311514/", textField:""},
+                {movieName:"Annihilation",yearReleased:2018, genre: "Adventure, Drama, Horror ", src:"https://m.media-amazon.com/images/M/MV5BMTk2Mjc2NzYxNl5BMl5BanBnXkFtZTgwMTA2OTA1NDM@._V1_UX182_CR0,0,182,268_AL_.jpg", wiki:"https://www.imdb.com/title/tt2798920/", textField:""},
+                    {movieName:"Back to the Future",yearReleased:19850, genre: "Adventure, Comedy, Sci-Fi", src:"https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UX182_CR0,0,182,268_AL_.jpg", wiki:"https://www.imdb.com/title/tt0088763/", textField:""}
             ]}/>
 
       </div>
--- forth_app/src/Netflix.js
+++ forth_app/src/Netflix.js
@@ -4,7 +4,47 @@ import React, {Component} from "react";
 
 
 class Netflix extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {DisplayMovieInfo: [],
+            gameTitle: "",
+            ageRating: ""}
+    }
+
+    gameTitleChange = (event) => {
+        this.setState({gameTitle: event.target.value})
+    };
+
+    ageRatingChange = (event) => {
+        this.setState({ageRating: event.target.value})
+    };
+
+    submitFunction = (event) =>{
+        console.log("Submitted Form.");
+        alert(this.state.gameTitle);
+        alert(this.state.ageRating);
+        //This prevent the form from reloading
+        event.preventDefault();
+    };
+
     render() {
+
+        // var dunkin = this.props.movieInfo.map(
+        //     eachItemTest => {
+        //
+        //         var image = eachItemTest.src;
+        //         var wikiLink = eachItemTest.wiki;
+        //
+        //         return(
+        //             <div className="GradeArray">
+        //                 <a href={wikiLink} target="_blank"> <img src={image}/></a>
+        //                 <p> _____ </p>
+        //             </div>
+        //         );
+        //     }
+        // );
+
 
         var DisplayMovies = this.props.movieInfo.map(
             eachItemTest => {
@@ -13,11 +53,18 @@ class Netflix extends Component {
                 var wikiLink = eachItemTest.wiki;
 
                 return(
-                    <div className="GradeArray">
+                    <div className="GradeArray" key={eachItemTest.movieName}>
                         <p> -Movie: {eachItemTest.movieName}</p>
                         <p> -Release Date: {eachItemTest.yearReleased}</p>
                         <p> -Genre: {eachItemTest.genre}</p>
-                        <a href={wikiLink} target="_blank"> <img src={image}/></a>
+                        <a href={wikiLink} target="_blank"> <img alt={image} src={image}/></a>
+                        <form onSubmit={this.submitFunction}>
+                            <h1>Game</h1>
+                            <label htmlFor="gameTitle">Game: </label>
+                            <input name="Game_Title_Information" title="Input a Game Title" type="Text" id="gameTitle" placeholder="Number" value={this.state.gameTitle} onChange={this.gameTitleChange}/>
+                            <input name="Game_Age_Rating" title="Input a Rating" type="Text" id="ageRating" placeholder="Rating" value={this.state.ageRating} onChange={this.ageRatingChange}/>
+                            <button name="Game_Button" title="Submit Button">Submit</button>
+                        </form>
                         <p> _____ </p>
                     </div>
                 );
@@ -26,7 +73,7 @@ class Netflix extends Component {
 
         return <div className="BankAccount">
 
-            <h1>{DisplayMovies}</h1>
+            <h3>{DisplayMovies}</h3>
         </div>
     }
 }
